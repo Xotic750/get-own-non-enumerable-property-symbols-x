4d48e618fe4ded88f3a4ee8ec988a84e85b18139
Graham Fairweather <xotic750@gmail.com>
2019-07-14 17:17:23 +0200

    Terraformed
    Automated build

---
+++ __tests__/get-own-non-enumerable-property-symbols-x.test.js
@@ -0,0 +1,113 @@
+let getOEPS;
+
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+
+  if (typeof JSON === 'undefined') {
+    JSON = {};
+  }
+
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  const es7 = require('es7-shim');
+  Object.keys(es7).forEach(function(key) {
+    const obj = es7[key];
+
+    if (typeof obj.shim === 'function') {
+      obj.shim();
+    }
+  });
+  getOEPS = require('../../index.js');
+} else {
+  getOEPS = returnExports;
+}
+
+const hasSymbolSupport = typeof Symbol === 'function' && typeof Symbol('') === 'symbol';
+
+let definedNonEnumerable;
+let definedEnumerable;
+
+if (hasSymbolSupport) {
+  let testObj = Object.defineProperty({}, Symbol('first'), {
+    enumerable: false,
+    value: 'first',
+  });
+
+  const nonEnum = Object.getOwnPropertySymbols(testObj);
+  // eslint-disable-next-line no-prototype-builtins
+  definedNonEnumerable = nonEnum.length === 1 && Object.propertyIsEnumerable(testObj, nonEnum) === false;
+
+  testObj = Object.defineProperty({}, Symbol('second'), {
+    enumerable: true,
+    value: 'second',
+  });
+
+  definedEnumerable = Object.getOwnPropertySymbols(testObj).length === 1;
+}
+
+const ifNonEnumerableSymbols = definedNonEnumerable ? it : xit;
+const ifEnumerableSymbols = definedEnumerable ? it : xit;
+
+describe('getOEPS', function() {
+  it('is a function', function() {
+    expect(typeof getOEPS).toBe('function');
+  });
+
+  it('should throw when target is null or undefined', function() {
+    expect(function() {
+      getOEPS();
+    }).toThrow();
+
+    expect(function() {
+      getOEPS(void 0);
+    }).toThrow();
+
+    expect(function() {
+      getOEPS(null);
+    }).toThrow();
+  });
+
+  it('should work with other primitives', function() {
+    const values = [1, true, 'ac'];
+
+    const expected = values.map(function() {
+      return [];
+    });
+
+    const actual = values.map(function(value) {
+      return getOEPS(value);
+    });
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('should return empty array', function() {
+    expect(getOEPS({bar: 1, foo: 2})).toStrictEqual([]);
+  });
+
+  ifNonEnumerableSymbols('should return non-enumerable own property symbols', function() {
+    const symbol = Symbol('first');
+    const obj = Object.defineProperty({bar: 1, foo: 2}, symbol, {
+      enumerable: false,
+      value: 'first',
+    });
+
+    Object.defineProperty(obj, Symbol('second'), {
+      enumerable: true,
+      value: 'second',
+    });
+
+    expect(getOEPS(obj)).toStrictEqual([symbol]);
+  });
+
+  ifEnumerableSymbols('should return non-enumerable own property symbols', function() {
+    const symbol = Symbol('first');
+    const obj = Object.defineProperty({bar: 1, foo: 2}, symbol, {
+      enumerable: true,
+      value: 'first',
+    });
+
+    expect(getOEPS(obj)).toStrictEqual([]);
+  });
+});
--- src/get-own-non-enumerable-property-symbols-x.js
+++ src/get-own-non-enumerable-property-symbols-x.js
@@ -7,17 +7,15 @@
  * @module get-own-non-enumerable-property-symbols-x
  */
 
-'use strict';
-
-var toObject = require('to-object-x');
-var filter = require('array-filter-x');
-var getOPS = require('get-own-property-symbols-x');
-var propertyIsEnumerable = require('property-is-enumerable-x');
+const toObject = require('to-object-x');
+const filter = require('array-filter-x');
+const getOPS = require('get-own-property-symbols-x');
+const propertyIsEnumerable = require('property-is-enumerable-x');
 
 /**
  * This method returns only the non-enumerable own property symbols of an object.
  *
- * @param {Object} target - The target.
+ * @param {object} target - The target.
  * @throws {typeError} - If target is null or undefined.
  * @returns {Array} The non-enumerable own property symbols.
  * @example
@@ -41,8 +39,9 @@ var propertyIsEnumerable = require('property-is-enumerable-x');
  */
 // eslint-disable-next-line id-length
 module.exports = function getOwnNonEnumerablePropertySymbols(target) {
-  var object = toObject(target);
-  return filter(getOPS(object), function (symbol) {
+  const object = toObject(target);
+
+  return filter(getOPS(object), function(symbol) {
     return propertyIsEnumerable(object, symbol) === false;
   });
 };
