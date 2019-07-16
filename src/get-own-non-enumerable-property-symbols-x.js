52aa6a03efd10eb9aee65103adbf922b237523a3
Graham Fairweather <xotic750@gmail.com>
2019-07-16 21:40:20 +0200

    Terraformed
    Auto build

--- src/get-own-non-enumerable-property-symbols-x.js
+++ src/get-own-non-enumerable-property-symbols-x.js
@@ -1,12 +1,3 @@
-/**
- * @file Like Object.getOwnPropertySymbols but gets only non-enumerable properties.
- * @version 1.0.0
- * @author Xotic750 <Xotic750@gmail.com>
- * @copyright  Xotic750
- * @license {@link <https://opensource.org/licenses/MIT> MIT}
- * @module get-own-non-enumerable-property-symbols-x
- */
-
 import toObject from 'to-object-x';
 
 import filter from 'array-filter-x';
@@ -19,24 +10,6 @@ import propertyIsEnumerable from 'property-is-enumerable-x';
  * @param {object} target - The target.
  * @throws {typeError} - If target is null or undefined.
  * @returns {Array} The non-enumerable own property symbols.
- * @example
- * var getOwnNonEnumerablePropertySymbols = require('get-own-non-enumerable-property-symbols-x');
- *
- * var obj = { bar: 1, foo: 2 };
- *
- * var symbol1 = Symbol('first');
- * Object.defineProperty(obj, symbo1l, {
- *   enumerable: false,
- *   value: 'first'
- * });
- *
- * var symbol2 = Symbol('second');
- * Object.defineProperty(obj, symbol2, {
- *   enumerable: true,
- *   value: 'second'
- * });
- *
- * getOwnNonEnumerablePropertySymbols(obj); // [symbol1]
  */
 // eslint-disable-next-line id-length
 export default function getOwnNonEnumerablePropertySymbols(target) {
