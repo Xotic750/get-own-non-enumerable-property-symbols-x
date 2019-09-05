5d8197568ff7fbd5aa2d6348dc3603b9c9b19101
Graham Fairweather <xotic750@gmail.com>
2019-09-05 02:00:38 +0200

    Maintenance

--- dist/get-own-non-enumerable-property-symbols-x.js
+++ dist/get-own-non-enumerable-property-symbols-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-30T22:35:11.626Z",
+  "date": "2019-09-05T00:00:31.369Z",
   "describe": "",
   "description": "Like Object.getOwnPropertySymbols but gets only non-enumerable properties.",
   "file": "get-own-non-enumerable-property-symbols-x.js",
